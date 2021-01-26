0dbd673602ed0832cf3f6f886d33763078b1aec5
Miguel Gonzalez <42626056+magonzalezo@users.noreply.github.com>
2021-01-26 10:48:13 -0500

    Delete special characters

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,7 @@
-def call(Map stageParams) {​​
+def call(Map stageParams) {
     checkout([
         $class: 'GitSCM',
         branches: [[name: stageParams.branch ]],
         userRemoteConfigs: [[ url: stageParams.url ]]
     ])
-}​​
+}
